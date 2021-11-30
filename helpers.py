27f4b9ab67b4aa6428ba9038965b9be79939a832
bwrob <bw2002@wp.pl>
2021-11-30 08:35:04 +0100

    [ADD] Merging individual ticker data to one dataframe

--- helpers.py
+++ helpers.py
@@ -2,6 +2,9 @@ import datetime
 import yfinance as yf
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import rrule, MONTHLY
+from functools import reduce
+import pandas as pd
+
 
 # 'January': 'F',
 # 'February': 'G',
@@ -16,21 +19,32 @@ from dateutil.rrule import rrule, MONTHLY
 # 'November': 'X',
 # 'December': 'Z'
 month_code_list = ('F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z')
-
+btc_str = 'BTC-USD'
 
 def get_ticker_codes(date=datetime.date.today(), next=24, prev=0):
     start = date + relativedelta(months=-prev)
     end = date + relativedelta(months=+next)
-    dates = [(dt.year, dt.month) for dt in rrule(MONTHLY, dtstart=start, until=end, bymonthday=-1)]
-    return ["BTC" + month_code_list[date[1] - 1] + str(date[0] % 100) + ".CME" for date in dates]
+    # determine all month ends in the period between start and end
+    rule = rrule(MONTHLY, dtstart=start, until=end, bymonthday=-1)
+    return ["BTC" + month_code_list[date.month - 1] + str(date.year % 100) + ".CME" for date in rule]
+    
+
+def get_ticker_close_data(code):
+    ticker = yf.Ticker(code)
+    history = ticker.history(period='max').reset_index()
+    if not history.empty:
+        history = history[['Date', 'Close']].rename(columns={'Close': code})
+    return history
 
 
-def get_ticker_history(ticker_codes):
-    history_list = []
+def get_close_data(ticker_codes):
+    data_list = []
     for code in ticker_codes:
-        ticker = yf.Ticker(code)
-        history = ticker.history(period='max')
-        if not history.empty:
-            history_list.append(history)
-            print("Data loaded for " + code)
-    return history_list
+        data = get_ticker_close_data(code)
+        if not data.empty:
+            data_list.append(data)
+    futures_frame = reduce(lambda df1, df2: pd.merge(df1, df2, how='outer', on='Date'), data_list)
+    bitcoin_frame = get_ticker_close_data(btc_str)
+    final_frame = pd.merge(futures_frame, bitcoin_frame, how='left', on='Date')
+    final_frame = final_frame.sort_values(by=['Date'], ascending=True)
+    return final_frame
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import helpers
+import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    codes = helpers.get_ticker_codes(prev=24)
-    historical_data = helpers.get_ticker_history(codes)
-    for df in historical_data:
-        print(df.head)
+    download_data = False
+    if download_data:
+        codes = helpers.get_ticker_codes(prev=24)
+        df = helpers.get_close_data(codes).to_csv('final.csv', index=False)
+    else:
+        df = pd.read_csv ('final.csv')
+    
+    print(df.head)
+    df.plot(x="Date", y=["BTC-USD", "BTCX21.CME", "BTCZ21.CME"])
+    plt.show()
